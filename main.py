1ebd89cb7d5f63c68d62a1e41fb1de41dd90b3e6
Robert Paulson <admin@altostrat.com>
2025-03-13 18:15:30 +0000

    main

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import json
-import time
-import requests
+import json, time, requests
 from flask import Flask, jsonify, request, abort
-from backend_handlers import discovery
 
 app = Flask(__name__)
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True)
