1e7a7a9880efab77227e311066e66b81e98bb03b
Robert Paulson <admin@altostrat.com>
2025-03-13 17:54:05 +0000

    test

--- main.py
+++ main.py
@@ -6,19 +6,6 @@ from backend_handlers import discovery
 
 app = Flask(__name__)
 
-# create the discovery, liveness and readiness endpoints
-@app.route('/discovery', methods=['GET'])
-def discovery_route():
-    return discovery()
-
-@app.route('/live', methods=['GET'])
-def liveness_route():
-    return jsonify({"status": "live", "code": 200, "timestamp": time.time()})
-
-@app.route('/ready', methods=['GET'])
-def readiness_route():
-    return jsonify({"status": "ready", "code": 200, "timestamp": time.time()})
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
