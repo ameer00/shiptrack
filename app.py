42f8e2b71588e4fd60c1e2e2abc86d4701455a33
Ameer Abbas <ameer00@gmail.com>
2025-04-24 06:21:29 -0700

    init

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
 import json
 from flask import Flask, jsonify, abort
-import time
 
-# Create Flask app instance
 app = Flask(__name__)
 
-@app.route('/discovery', methods=['GET'])
-def discovery():
-    return jsonify({
-        "name": "shipping",
-        "version": "1.0",
-        "owners": ["ameerabb", "lonestar"],
-        "team": "genAIs",
-        "organization": "acme"
-    })
 
-# create liveness and readiness endpoints
-@app.route('/liveness', methods=['GET'])
-def liveness():
-    return jsonify({"status": "live", "code": 200, "timestamp": time.time()})
-
-@app.route('/readiness', methods=['GET'])
-def readiness():
-    return jsonify({"status": "ready", "code": 200, "timestamp": time.time()})
-    
 
 if __name__ == '__main__':
-    # Run the Flask app
-    app.run(debug=True) # Use debug=False in production
+    app.run(debug=True, port=5000) # Run the app on port 5000
