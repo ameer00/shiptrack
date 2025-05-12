004e758e3d799afc551fe0aadd08a052cb30c27d
Ameer Abbas <ameer00@gmail.com>
2025-05-12 08:53:07 -0700

    init

--- tmp/main.py
+++ tmp/main.py
@@ -3,17 +3,7 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/discovery', methods=['GET'])
-def discovery():
-    return jsonify({
-        "name": "shipping",
-        "version": "1.0",
-        "owners": ["ameerabb", "lonestar"],
-        "team": "genAIs",
-        "organization": "acme"
-    })
 
-# 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
